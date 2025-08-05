26c248e6088d9ef244004fc5c5ee5d353fe36c70
gabyjahns <jahns.gabriely@escola.pr.gov.br>
2025-08-05 10:48:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Conhecimentos gerais',
+    'Quais as duas línguas mais faladas no mundo?',
+    ' Inglês e o chinês mandarim'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual o maior animal terrestre?',
+    'Elefante africano.'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    ' De quem é a famosa frase “Penso, logo existo”?',
+    ' René Descartes '
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual a montanha mais alta do mundo?',
+    'Monte Everest'
+)
